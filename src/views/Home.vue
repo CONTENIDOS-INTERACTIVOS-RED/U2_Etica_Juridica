8a2e644b0abf8a1615b1508bad58a56a88ca19f7
Francisco Lizcano <flizcanor@icloud.com>
2025-06-12 20:43:56 -0500

    se configuran estilos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,11 +22,9 @@
             .content-wrapper.px-lg-4.pt-lg-4.px-xxl-5.py-xxl-5.text-start
               p.home-main-title UNIDAD {{ `${globalData.numeroUnidad}` }}
               h1.home-title.mb-4 {{ globalData.tituloUnidad }}
-              p.descripcion.mb-4 Desde los comienzos de la humanidad, la relación entre los principios éticos, el desarrollo económico y la sostenibilidad ha sido fundamental para garantizar un progreso equilibrado y equitativo. En un contexto globalizado, donde los desafíos éticos y ambientales son cada vez más evidentes, la bioética emerge como una herramienta clave para orientar decisiones económicas responsables que respeten tanto el medio ambiente como los derechos humanos.
-
-              p.descripcion.mb-4 En esta unidad, profundizaremos en el rol de la bioética como pilar de la sostenibilidad en el desarrollo económico, explorando temas esenciales como la aplicación de principios éticos al uso de recursos naturales, la influencia de las políticas globales en la protección de los derechos humanos y la ética en la innovación tecnológica. 
+              p.descripcion.mb-4 Desde los orígenes de la civilización, la reflexión sobre lo correcto y lo justo ha sido fundamental para la construcción de una sociedad armoniosa. En esta unidad, se profundiza en los conceptos de ética y moral, sus raíces filosóficas y su aplicación en el ejercicio del Derecho. A través del análisis del Decálogo del Abogado, se busca formar profesionales íntegros, comprometidos con la justicia, la responsabilidad y los valores que sustentan la profesión jurídica.
               
-              p #[b Prepárate para explorar los retos y oportunidades que la sostenibilidad y la bioética presentan en un mundo en constante evolución.]
+              p #[b ¡Prepárese para ejercer el Derecho con ética, criterio y compromiso profesional!]
 
               .button-container.mt-auto
                 router-link.btn-iniciar.w-100.w-md-auto(:to="{ name: 'introduccion' }")
@@ -150,11 +148,11 @@ export default {
   .iniciar-btn-text
     transition: all 0.25s ease-in-out
     background-color: $color-acento-botones !important
-    color: #0B0A0A !important
+    color: #fff !important
   .icon
     transition: all 0.25s ease-in-out
     // background-color: #083E61 !important
-    background-color: darken($color-acento-botones, 30% ) !important
+    background-color: #4A5641 !important
   &:hover
     .icon
       transform: scale(1.06)
